82d05091ff3579311d158371ab56c9e334aea267
kayanabdullah00-eng <kayanabdullahabdullah00@gmail.com>
2026-03-08 20:39:21 +0000

    Add function _memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _memcpy - copies n bytes from memory area src to memory area dest
- * @dest: destination memory area
- * @src: source memory area
+ * _memcpy - copies memory area
+ * @dest: destination buffer
+ * @src: source buffer
  * @n: number of bytes to copy
  *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
-    }
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
 
-    return dest;
+	return (dest);
 }
