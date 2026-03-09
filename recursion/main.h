62700c729e62809569b47a7d14ac7a690db2c8b1
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-03-09 18:24:16 +0000

    Add puts recursion function

--- recursion/main.h
+++ recursion/main.h
@@ -1,12 +1,7 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	_puts_recursion("Puts with recursion");
-	return (0);
-}
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
