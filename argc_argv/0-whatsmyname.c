0527d69abcbe4eba2e3abb6a3b12c9c5d20288f9
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-03-10 17:22:04 +0000

    fix 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
-    return (0);
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
