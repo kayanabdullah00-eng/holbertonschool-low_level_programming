2903bda9907a817ee53b17465b2c29bc42cbcc9a
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-03-30 13:18:05 +0000

    Fix Betty style again

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,26 +5,26 @@
  * create_array - creates an array of chars, and initializes it
  * @size: size of the array
  * @c: char to initialize with
+ *
  * Return: pointer to array, or NULL if it fails
  */
 char *create_array(unsigned int size, char c)
 {
-    char *p;
-    unsigned int i;
+	char *p;
+	unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    p = malloc(sizeof(char) * size);
+	p = malloc(sizeof(char) * size);
 
-    if (p == NULL)
-        return (NULL);
+	if (p == NULL)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-    {
-        p[i] = c;
-    }
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
 
-
-    return (p);
+	return (p);
 }
