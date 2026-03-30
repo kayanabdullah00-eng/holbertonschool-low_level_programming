af34d158c0ae56eb85980be6041518bbd2702a07
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-03-30 13:09:48 +0000

    Fix syntax errors

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it
+ * @size: size of the array
+ * @c: char to initialize with
+ * Return: pointer to array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+    char *p;
+    unsigned int i;
+
+    if (size == 0)
+        return (NULL);
+
+    p = malloc(sizeof(char) * size);
+
+    if (p == NULL)
+        return (NULL);
+
+    for (i = 0; i < size; i++)
+    {
+        p[i] = c;
+    }
+
+    return (p);
+}
+
