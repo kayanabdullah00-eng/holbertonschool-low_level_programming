dc41cb8236519899c3a2f01319e28f69371fdb64
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-01 12:41:11 +0000

    Add main.h and task 0 malloc_checked

---
+++ more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file: main.h
+ * description: Header file containing prototypes for all functions
+ * used in the 0x0c-more_malloc_free directory.
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif
