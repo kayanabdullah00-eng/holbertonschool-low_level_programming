21a04a8878d565861b4db66e3e448ea2ed216092
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-05 14:37:40 +0000

    Fix all Betty style and documentation issues

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,19 +1,26 @@
 #include "3-calc.h"
+#include <stdlib.h>
 
+/**
+ * get_op_func - selects the correct function to perform the operation
+ * @s: the operator passed as argument
+ *
+ * Return: pointer to the function that corresponds to the operator
+ */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op != NULL && *(ops[i].op) != *s)
-        i++;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
-    return (ops[i].f);
+	return (ops[i].f);
 }
