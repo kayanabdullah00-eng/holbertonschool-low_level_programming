def03b87f7dc5c7b7e0a323f55cd66eb6689a40f
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-05 14:10:10 +0000

    Remove non-code text and fix compilation error

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,16 +1,18 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * array_iterator - executes a function on each element of an array
- * @array: the array
- * @size: size of the array
- * @action: pointer to the function to use
+ * @array: the array to iterate over
+ * @size: the size of the array
+ * @action: a pointer to the function you need to use
+ *
+ * Return: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	(Correct output - case: NULL)
 	if (array == NULL || action == NULL)
 		return;
 
