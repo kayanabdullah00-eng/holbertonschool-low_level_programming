33e6f5056558ba0b94a0a025cf4db833a7d49b09
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-05 14:31:29 +0000

    Fix Betty style errors in 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,34 +1,41 @@
 #include "3-calc.h"
 
+/**
+ * main - prints the result of simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, or exit with status on failure
+ */
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    int (*func)(int, int);
+	int num1, num2;
+	int (*func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    func = get_op_func(argv[2]);
+	func = get_op_func(argv[2]);
 
-    if (func == NULL || argv[2][1] != '\0')
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (func == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    printf("%d\n", func(num1, num2));
+	printf("%d\n", func(num1, num2));
 
-    return (0);
+	return (0);
 }
