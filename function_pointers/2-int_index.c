2a74be5054a3a1eca180949b0940c0ae8029b109
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-05 14:22:00 +0000

    Final fix for int_index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,6 +1,13 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
+/**
+ * int_index - searches for an integer
+ * @array: array to search in
+ * @size: size of the array
+ * @cmp: pointer to the comparison function
+ *
+ * Return: index of the first element, or -1 if no match or size <= 0
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -8,3 +8,4 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
+int int_index(int *array, int size, int (*cmp)(int));
