8efaab255d5b9d4a0985e1f1c55b03a1aa74ca46
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-05 13:51:51 +0000

    Fix Betty style spaces and tabs

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,17 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
- * print_name - prints a name using a function pointer
- * @name: the name to print
- * @f: pointer to function
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer to the printing function
+ *
+ * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-        f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
