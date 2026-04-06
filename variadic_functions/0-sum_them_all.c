431a25826fd521c5aaa31a1f7c005c537dc37532
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-06 13:06:36 +0000

    Fix Betty documentation

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,5 +1,13 @@
 #include "variadic_functions.h"
 
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: the number of parameters passed to the function
+ * @...: a variable number of parameters to calculate the sum of
+ *
+ * Return: if n == 0 - 0
+ * otherwise - the sum of all parameters
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
