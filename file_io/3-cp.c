c93dbc184b52ac3c7d5de4c45281220351ca8040
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-22 13:52:54 +0000

    k

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,30 +10,30 @@
  * @ac: argument count
  * @av: argument vector
  *
- * Return: 0 on success, exit codes on failure
+ * Return: 0 on success, exits with codes on failure
  */
 int main(int ac, char **av)
 {
-	int fd_from, fd_to, r, w;
+	int fd_from, fd_to, r, w, close_check;
 	char buffer[BUF_SIZE];
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		close(fd_from);
 		exit(99);
 	}
@@ -43,7 +43,7 @@ int main(int ac, char **av)
 		w = write(fd_to, buffer, r);
 		if (w == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -52,22 +52,23 @@ int main(int ac, char **av)
 
 	if (r == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		close(fd_from);
 		close(fd_to);
 		exit(98);
 	}
 
-	if (close(fd_from) == -1)
+	close_check = close(fd_from);
+	if (close_check == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		close(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 
-	if (close(fd_to) == -1)
+	close_check = close(fd_to);
+	if (close_check == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 
