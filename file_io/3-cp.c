7519e805437971f11de6cac951c4ea32e1e1c3c3
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-22 13:56:49 +0000

    k

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,17 +5,15 @@
 
 #define BUF_SIZE 1024
 
+void close_fd(int fd);
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to);
+
 /**
- * main - copies file content to another file
- * @ac: argument count
- * @av: argument vector
- *
- * Return: 0 on success, exits with codes on failure
+ * main - copies file content
  */
 int main(int ac, char **av)
 {
-	int fd_from, fd_to, r, w, close_check;
-	char buffer[BUF_SIZE];
+	int fd_from, fd_to;
 
 	if (ac != 3)
 	{
@@ -38,12 +36,28 @@ int main(int ac, char **av)
 		exit(99);
 	}
 
+	copy_file(fd_from, fd_to, av[1], av[2]);
+
+	close_fd(fd_from);
+	close_fd(fd_to);
+
+	return (0);
+}
+
+/**
+ * copy_file - copies content
+ */
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+	char buffer[BUF_SIZE];
+	ssize_t r, w;
+
 	while ((r = read(fd_from, buffer, BUF_SIZE)) > 0)
 	{
 		w = write(fd_to, buffer, r);
 		if (w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -52,25 +66,23 @@ int main(int ac, char **av)
 
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		close(fd_from);
 		close(fd_to);
 		exit(98);
 	}
+}
 
-	close_check = close(fd_from);
-	if (close_check == -1)
+/**
+ * close_fd - closes file descriptor safely
+ */
+void close_fd(int fd)
+{
+	int c = close(fd);
+
+	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-
-	close_check = close(fd_to);
-	if (close_check == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
-	return (0);
 }
