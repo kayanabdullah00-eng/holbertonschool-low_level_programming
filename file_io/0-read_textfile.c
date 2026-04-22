9582cdaf008bbe4c0a7d2923ebccf1ce060c875b
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-22 13:26:30 +0000

    0. read_textfile task - Betty compliant

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,42 +12,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t r, w;
-    char *buffer;
+	int fd;
+	ssize_t r, w;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    r = read(fd, buffer, letters);
-    if (r == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	r = read(fd, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    w = write(STDOUT_FILENO, buffer, r);
-    if (w == -1 || w != r)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1 || w != r)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    free(buffer);
-    close(fd);
+	free(buffer);
+	close(fd);
 
-    return (r);
+	return (r);
 }
