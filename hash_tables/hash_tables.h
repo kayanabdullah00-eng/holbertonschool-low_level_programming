11fa094eea502451cafb94c13087555b18e8e049
“kayanabdullah00 <kayanabdullahabdullah00@gmail.com>
2026-04-26 13:16:21 +0000

    k

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,32 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
+
+/**
+ * struct hash_node_s - hash node structure
+ * @key: key
+ * @value: value
+ * @next: pointer to the next node
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - hash table structure
+ * @size: size of the array
+ * @array: array of linked lists
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
